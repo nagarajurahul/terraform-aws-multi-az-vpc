722c0768f5fd2113c635338b79c8d39acaaa3195
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-06 13:24:29 -0600

    no need for lower case validation for region as regex validation exists

--- variables.tf
+++ variables.tf
@@ -11,9 +11,4 @@ variable "region"{
       condition = can(regex("^[a-z]{2}-[a-z]+-[0-9]{1}$", var.region))
       error_message = "Please enter a valid AWS region (e.g., us-east-1, us-west-2, eu-central-1)"
     }
-
-    validation{
-        condition = lower(var.region) == var.region
-        error_message = "Please follow lower case for region names"
-    }
 }
