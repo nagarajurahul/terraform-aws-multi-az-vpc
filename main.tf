fac32bed860feaf7cc7e1a153c477b6eb3b3104e
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-06 15:54:15 -0600

    update variables to ask number of devices per subnet

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ data "aws_regions" "current"{
 
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "main"{
     cidr_block = var.vpc_cidr
     tags = {
@@ -13,7 +17,7 @@ resource "aws_vpc" "main"{
     }
 }
 
-resource "aws_subnet" "public"{
+resource "aws_subnet" "private"{
     vpc_id = aws_vpc.main.id
     # cidr_block = 
 }
--- variables.tf
+++ variables.tf
@@ -12,3 +12,19 @@ variable "region"{
       error_message = "Please enter a valid AWS region (e.g., us-east-1, us-west-2, eu-central-1)"
     }
 }
+
+variable "number_of_devices_per_subnet"{
+    description = "Number of devices to support in each subnet"
+    type = Number
+    default = 249
+}
+
+locals {
+    usable_ips = var.number_of_devices_per_subnet + 5 # Add 5 for reserved IPs by AWS
+
+    subnet_mask = (
+    local.usable_ips <= 254 ? 24 : # 256 total IPs, 254 usable
+    local.usable_ips <= 510 ? 23 : # 512 total IPs, 510 usable
+    local.usable_ips <= 1022 ? 22 : 21 # 1024 total IPs, 1022 usable
+  )
+}
