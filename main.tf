837172532f5e408b561bc3b1544e00e031a39f37
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-06 16:03:02 -0600

    add comments and generate cidrs for the public and private subnets

--- main.tf
+++ main.tf
@@ -6,10 +6,16 @@ data "aws_regions" "current"{
 
 }
 
+# Pull avialable azs dynamically from aws for current region
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
+locals{
+    az_count = count(data.aws_availability_zones.available.names)
+}
+
+# Create aws vpc resource
 resource "aws_vpc" "main"{
     cidr_block = var.vpc_cidr
     tags = {
@@ -17,7 +23,10 @@ resource "aws_vpc" "main"{
     }
 }
 
+# Maximum number of supported devices per subnet are 2041
+# Minimum is 249
 
+# Calculate the subnet mask
 locals {
     usable_ips = var.number_of_devices_per_subnet + 5 # Add 5 for reserved IPs by AWS
 
@@ -28,7 +37,19 @@ locals {
   )
 }
 
-resource "aws_subnet" "private"{
-    vpc_id = aws_vpc.main.id
-    # cidr_block = 
+# Generate cidrs for public and private subnets
+locals{
+  private_subnet_cidrs = [
+    for i in range(data.az_count) : cidrsubnet(var.vpc_cidr, local.subnet_mask - 16, i + 1)
+  ]
+
+  public_subnet_cidrs = [
+    for i in range(data.az_count) : cidrsubnet(var.vpc_cidr, local.subnet_mask - 16, i + data.az_count + 1)
+  ]  
 }
+
+
+# resource "aws_subnet" "private"{
+#     vpc_id = aws_vpc.main.id
+#     # cidr_block = 
+# }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "vpc_cidr"{
     type = string
+    description = "CIDR for the VPC"
     default = "10.0.0.0/16"
 }
 
