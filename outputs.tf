cc28dcae13a604b51778fb68276783322aa3cfea
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-06 16:10:20 -0600

    output cidrs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value = aws_vpc.main
 }
+
+output "private_subnet_cidrs"{
+    description = "CIDRs for the private subnets"
+    value = local.private_subnet_cidrs
+}
+
+output "public_subnet_cidrs"{
+    description = "CIDRs for the public subnets"
+    value = local.public_subnet_cidrs
+}
