59099d81c7fd04b9d3505a8a8d5f133555adb850
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-09 12:32:03 -0500

    added tags for subnets

--- main.tf
+++ main.tf
@@ -18,8 +18,9 @@ locals{
 # Create aws vpc resource
 resource "aws_vpc" "main"{
     cidr_block = var.vpc_cidr
+
     tags = {
-       "Name" = "multi-az-vpc" 
+       "Name" = var.vpc_name
     }
 }
 
@@ -51,14 +52,24 @@ locals{
 
 resource "aws_subnet" "private"{
     count = local.az_count
+
     vpc_id = aws_vpc.main.id
     cidr_block = local.private_subnet_cidrs[count.index]
     availability_zone = element(data.aws_availability_zones.available.names,count.index)
+
+    tags={
+      Name = "${var.vpc_name}-private-${count.index}"
+    }
 }
 
 resource "aws_subnet" "public"{
     count = local.az_count
+
     vpc_id = aws_vpc.main.id
     cidr_block = local.public_subnet_cidrs[count.index]
     availability_zone = element(data.aws_availability_zones.available.names,count.index)
+
+    tags={
+        Name = "${var.vpc_name}-public-${count.index}"
+    }
 }
