229fac33df96ce883efa00ba20d30869ddebdb8d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-10 08:14:33 -0500

    add route table for private subnet

--- main.tf
+++ main.tf
@@ -122,3 +122,12 @@ resource "aws_nat_gateway" "nat"{
     Name = "${var.vpc_name}-nat"
   }
 }
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
