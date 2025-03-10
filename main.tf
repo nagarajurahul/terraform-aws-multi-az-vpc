476329590f69fe85e25005f50d48d6a920379ac8
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-10 08:15:49 -0500

    added tags for private rt

--- main.tf
+++ main.tf
@@ -130,4 +130,8 @@ resource "aws_route_table" "private" {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
 }
