ce28abf251ab8a1ddf73b4c7aad33792fb6ad65d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-11 21:41:49 -0500

    re-arranged outputs

--- outputs.tf
+++ outputs.tf
@@ -3,23 +3,28 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "private_subnet_cidrs"{
-    description = "CIDRs for the private subnets"
-    value = local.private_subnet_cidrs
-}
-
-output "public_subnet_cidrs"{
-    description = "CIDRs for the public subnets"
-    value = local.public_subnet_cidrs
-}
-
 output "vpc_cidr_block" {
   description = "CIDR block of the VPC"
   value       = aws_vpc.main.cidr_block
 }
 
+output "private_subnet_cidrs"{
+    description = "CIDRs for the private subnets"
+    value = local.private_subnet_cidrs
+
+    sensitive   = true
+}
+
+output "public_subnet_cidrs"{
+    description = "CIDRs for the public subnets"
+    value = local.public_subnet_cidrs
+
+    sensitive   = true
+}
+
 output "subnet_ids" {
   description = "IDs of the subnets"
   value       = aws_subnet.private.*.id
+
   sensitive   = true
 }
