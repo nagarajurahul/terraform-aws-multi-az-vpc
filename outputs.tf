a4935be009074831dfb7b779a49dcdbf2979a7d4
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-11 21:41:17 -0500

    add vpc cidr and subnet ids to outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,14 @@ output "public_subnet_cidrs"{
     description = "CIDRs for the public subnets"
     value = local.public_subnet_cidrs
 }
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "subnet_ids" {
+  description = "IDs of the subnets"
+  value       = aws_subnet.private.*.id
+  sensitive   = true
+}
