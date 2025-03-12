09857c4f248370e73a884c263d674d7ea75a0987
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-03-11 21:34:29 -0500

    correct syntax for nat in private route tables

--- routes.tf
+++ routes.tf
@@ -58,7 +58,7 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat[0].id
   }
 
   tags= merge(
