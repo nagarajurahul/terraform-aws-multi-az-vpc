b0165a33f335912a7f3e29b7eb4cbea29a706208
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-02-10 20:06:06 -0600

    fix: ipv4 cidr calculation dynamically

--- subnets.tf
+++ subnets.tf
@@ -7,7 +7,32 @@
 #     az_count = length(data.aws_availability_zones.available.names)
 # }
 
-# Maximum number of devices allocated per subnet will be 2043
+locals {
+  az_count = length(local.private_azs)
+
+  # Counts number of IPs required per subnet as per devices specified in variables input
+  private_subnet_ips = pow(2, 32 - local.private_subnet_mask)
+
+  # Counts number of IPs for all private AZs combined
+  required_private_tier_ips = local.az_count * local.private_subnet_ips
+}
+
+locals {
+  private_tier_mask = (
+    local.required_private_tier_ips <= 4096  ? 20 :
+    local.required_private_tier_ips <= 8192  ? 19 :
+    local.required_private_tier_ips <= 16384 ? 18 : 17
+  )
+}
+
+locals {
+  # Split VPC into large, non-overlapping tiers
+  # 16 is VPC cidr range
+  public_tier_cidr  = cidrsubnet(var.vpc_cidr, 20-16, 0) # 10.0.0.0/20 -> 4096 IPs for public subnets
+  private_tier_cidr = cidrsubnet(var.vpc_cidr, local.private_tier_mask - 16, 1)
+}
+
+# Maximum number of devices allocated per subnet will be 4091
 # Minimum number of devices allocated will be 251
 
 # Calculate the subnet mask
@@ -17,14 +42,14 @@ locals {
     subnet_mask = (
     local.usable_ips <= 256 ? 24 : # 256 total IPs, 251 usable
     local.usable_ips <= 512 ? 23 : # 512 total IPs, 507 usable
-    local.usable_ips <= 1024 ? 22 : 21 # 1024 total IPs, 1019 usable
+    local.usable_ips <= 1024 ? 22 : # 1024 total IPs, 1019 usable
+    local.usable_ips <= 2048 ? 21 :20 # 2048 total IPs, 2043 usable
     )
     
+    # At most, in highly available production env, we may use 256 IPs per AZ, which is 2pow8
     public_subnet_mask  = 24
+    # Dynamic based on input
     private_subnet_mask = local.subnet_mask
-
-    public_newbits  = local.public_subnet_mask - 16
-    private_newbits = local.private_subnet_mask - 16
 }
 
 locals {
@@ -35,12 +60,15 @@ locals {
 
 # Generate cidrs for public and private subnets
 locals{
+  public_newbits  = local.public_subnet_mask  - 20
+  private_newbits = local.private_subnet_mask - local.private_tier_mask
+
   private_subnet_cidrs = {
-    for i,value in local.private_azs : value => cidrsubnet(var.vpc_cidr, local.private_newbits, i + 1)
+    for i,value in local.private_azs : value => cidrsubnet(local.private_tier_cidr, local.private_newbits, i)
   }
 
   public_subnet_cidrs = {
-    for i,value in local.public_azs :  value => cidrsubnet(var.vpc_cidr, local.public_newbits, i + length(local.private_azs) + 1)
+    for i,value in local.public_azs :  value => cidrsubnet(local.public_tier_cidr, local.public_newbits, i)
   }  
 }
 
